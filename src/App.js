3ddd34e38c5ad8d0d06e5a722fd8837df3696f3b
Shanthi-rajendran-22 <shanthi.r@nineleaps.com>
2020-08-29 01:46:38 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default class App extends React.Component {
+  render(){
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+  <div id="quote-box">
+   <div className="container">
+    <p id="text">
+      <i class="fa fa-quote-left" aria-hidden="true">
+        </i> Sometimes when you innovate, you make mistakes.
+         It is best to admit them quickly, and get on with improving your other innovations. <i class="fa fa-quote-right">
+      </i></p>
+    <p id="author">- Steve Jobs</p>
+    <div>
+     <button id="new-quote">Generate</button>
     </div>
+    <a href="twitter.com/intent/tweet" className="tweet" title="Tweet this quote"><i class="fab fa-twitter"></i></a>
+   </div>
+</div>
   );
 }
+}
 
-export default App;
+
